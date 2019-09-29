afe20ecaac2c21515febc746bf3b4d7c006752b9
VH <VH@DESKTOP-0FCNPNM>
2019-09-29 23:27:54 +0300

    Class Bye added

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,6 +3,8 @@ package hello;
 public class HelloWorld {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
+        ByeClass bye = new ByeClass();
         System.out.println(greeter.sayHello());
+        System.out.println(bye.sayBye());
     }
 }
